a8af20541787b4f2ae9b4ba41375edf2b1e15be6
Ajay Sontakke <sontakkeajay05@gmail.com>
2026-08-04 21:54:02 +0530

    Create project package structure and enums

---
+++ campussync-backend/src/main/java/com/campussync/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.campussync.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ campussync-backend/src/main/java/com/campussync/enums/Role.java
@@ -0,0 +1,7 @@
+package com.campussync.enums;
+
+public enum Role {
+    ADMIN,
+    TEACHER,
+    STUDENT
+}
