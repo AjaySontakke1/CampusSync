9587c1a0f6dabf75f313a39560919068e636690f
Ajay Sontakke <sontakkeajay05@gmail.com>
2026-08-04 21:22:30 +0530

    Initial commit: Setup CampusSync backend and frontend

---
+++ campussync-backend/src/main/java/com/campussync/CampusSyncApplication.java
@@ -0,0 +1,13 @@
+package com.campussync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusSyncApplication.class, args);
+    }
+
+}
